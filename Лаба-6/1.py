899045e68f3e0f270bf409cd908d0a0bf1c71b9f
viktoralso <73271227+viktoralso@users.noreply.github.com>
2020-12-06 14:47:49 +0900

    Add files via upload

---
+++ Лаба-6/1.py
@@ -0,0 +1,11 @@
+class Animal:
+	Name="ball"
+	FavFoodAnimal="catsMeat"
+	BirthDate="07.09.2007"
+class Dog(Animal):
+	NameDog="ball"
+	FavFoodDog="catsMeat"
+	BirthDateDog="07.09.2007"
+print("My dogs name is "+Dog.NameDog+". His favorite food is " +Dog.FavFoodDog)
+print( )
+print("Balls date of birth is "+Dog.BirthDateDog)
